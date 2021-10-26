6201b317d6894f3bdc13b622911a8dca030d30c0
Ростислав Александрович Боржонов <80468507+Wh1sky666@users.noreply.github.com>
2021-10-26 14:47:25 +0300

    26.10

--- individ.py
+++ individ.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def check(s):
-    result = 0
-    f = 0
-    for a in s:
-        if "(" in a:
-            result += 1
-        elif ")" in a:
-            result -= 1
-        if "[" in a:
-            result += 1
-        elif "]" in a:
-            result -= 1
 
-        else:
-            return "Правильное расположение скобок"
-    if result < 0:
-        return "Неправильное расположение скобок"
+def check(S, start=0, current=0):
 
-    if result > 0:
-        return "Неправильное расположение скобок"
-    return "Правильное расположение скобок"
+    if start == len(S):
+        return current == 0
+
+    if current < 0:
+        return False
+
+    if S[start] == "(" or S[start] == "[":
+        return check(S, start + 1, current + 1)
+
+    elif S[start] == ")" or S[start] == "]":
+        return check(S, start + 1, current - 1)
 
 
 if __name__ == '__main__':
-    s = input()
-    print(check(s))
+
+    S = ["(", ")", "(", ")", "[", "["]
+    print(check(S))
+
